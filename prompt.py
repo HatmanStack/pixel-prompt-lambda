a0c2cf9d062228d4d3b6a4d4809bae9264897270
HatmanStack <hatmanstack@protonmail.com>
2025-01-31 14:21:07 -0600

    changed prompt provider

--- prompt.py
+++ prompt.py
@@ -1,14 +1,40 @@
 import requests
 import json
-from config import API_URL, headers, parameters, options
+from groq import Groq
+from config import magic_prompt_model, API_URL, headers, parameters, prompt_model, groq_api_key, options, token
+
+groqClient = Groq(api_key=groq_api_key)
+
+prompt_base = 'Instructions:\
+1. Take the provided seed string as inspiration.\
+2. Generate a prompt that is clear, vivid, and imaginative.\
+3. This is a visual image so any reference to senses other than sight should be avoided.\
+4. Ensure the prompt is between 90 and 100 tokens.\
+5. Return only the prompt.\
+Format your response as follows:\
+[Your prompt here]\
+Remember:\
+- The prompt should be descriptive.\
+- Avoid overly complex or abstract phrases.\
+- Make sure the prompt evokes strong imagery and can guide the creation of visual content.\
+- Make sure the prompt is between 90 and 100 tokens.'
+
+prompt_assistant = "I am ready to return a prompt that is between 90 and 100 tokens.  What is your seed string?"
 
 def getPrompt(prompt, attempts=1):
     response = {}
     try:
-        prefix = f'enhance prompt: {prompt}'
-        apiData = {"inputs": prompt, "parameters": parameters, "options": options, "timeout": 45}
-        response = requests.post(API_URL + "succinctly/text2image-prompt-generator", headers=headers, data=json.dumps(apiData))
-        return response.json()
+        chat = [
+                {"role": "user", "content": prompt_base},
+                {"role": "assistant", "content": prompt_assistant},
+                {"role": "user", "content": prompt},
+            ]
+        response = groqClient.chat.completions.create(
+            messages=chat, temperature=1, max_tokens=2048, top_p=1, stream=False, stop=None, model=prompt_model
+        )
+        print(response)
+        return response.choices[0].message.content
+        
     except Exception as e:
         print(f"An error occurred: {e}")
         if attempts < 3:
@@ -18,6 +44,6 @@ def getPrompt(prompt, attempts=1):
 def inferencePrompt(itemString):
     try:
         response = getPrompt(itemString)
-        return {"plain": response[0]["generated_text"]}
+        return {"plain": response}
     except Exception as e:
         return {"plain": f'An Error occurred: {e}'}
